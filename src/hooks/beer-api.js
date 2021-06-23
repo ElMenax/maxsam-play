df09bfaa8fd11ec61cee50cd8520a6e574a8abc0
Maxime DULIN <maxime.dulin@kry.se>
2021-06-23 15:15:57 +0200

    WIP

---
+++ src/hooks/beer-api.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export const useBeerApi = () => {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const fetchMyBeers = async () => {
+      return await fetch('https://api.punkapi.com/v2/beers');
+    };
+
+    fetchMyBeers()
+      .then((payload) => payload.json())
+      .then(
+        (payload) => {
+          setBeers(
+            payload.map((item) => ({
+              id: item.id,
+              name: item.name,
+              image: item.image_url,
+            }))
+          );
+          //console.log(payload);
+        },
+        (error) => console.log('Something went wrong')
+      );
+  }, []);
+
+  return { beers };
+};
