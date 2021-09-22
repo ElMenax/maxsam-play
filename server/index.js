0ae278072b198825bbe310e3cfbcaa9bbd9355d8
Maxime DULIN <maxime.dulin@kry.se>
2021-09-22 14:58:30 +0200

    Add graphQL

--- server/index.js
+++ server/index.js
@@ -1,11 +1,63 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
 const axios = require('axios');
 const cors = require('cors');
+const { graphql, buildSchema } = require('graphql');
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
+
+const schema = buildSchema(`
+  type BeerImage {
+    url: String
+  }
+  type Beer {
+    name: String
+    id: Int
+    image: BeerImage
+    alcohol: Float
+  }
+  type Query {
+    beers: [Beer]
+    beer(id: Int!): Beer
+  }
+`);
+
+const root = {
+  beers: async () => {
+    const beers = await getBeers();
+    return beers;
+  },
+  beer: async ({ id }) => {
+    const beers = await getBeers();
+    return beers.find((b) => b.id === id);
+  },
+};
+
+const getBeers = () => {
+  const beersPromise = axios.get('https://api.punkapi.com/v2/beers');
+
+  return beersPromise.then(({ data }) => {
+    return data.map((item) => ({
+      id: item.id,
+      name: item.name,
+      image: { url: item.image_url },
+      alcohol: item.abv,
+    }));
+  });
+};
+
+const getWeather = () => {
+  const weatherPriomise = axios.get(
+    'https://www.metaweather.com/api/location/906057/'
+  );
+
+  return weatherPriomise.then(({ data }) => {
+    return data.consolidated_weather[0];
+  });
+};
 
 const parseBeers = (beers, weather) => {
   return beers
@@ -19,6 +71,15 @@ const parseBeers = (beers, weather) => {
       alcohol: item.abv,
     }));
 };
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.get('/beers', (req, res) => {
   const beersPromise = axios.get('https://api.punkapi.com/v2/beers');
